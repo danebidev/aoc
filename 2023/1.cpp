4737bc999b4efbed030c3d093c889176252e409d
danebidev <57720273+danebidev@users.noreply.github.com>
2023-12-02 16:48:35 +0100

    Switched to C++ for 2023

---
+++ 2023/1.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ifstream in("input");
+
+    int ans = 0;
+    string line;
+
+    while (getline(in, line)) {
+    }
+
+    cout << ans;
+
+    return 0;
+}
