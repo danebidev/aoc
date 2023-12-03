66c257d52726c45b646e6f1590f87a4f7f2e85ce
danebidev <57720273+danebidev@users.noreply.github.com>
2023-12-03 01:26:37 +0100

    Added first part to 2023/day 1

--- 2023/1.cpp
+++ 2023/1.cpp
@@ -8,17 +8,28 @@ using namespace std;
 int main() {
     ifstream in("input");
 
-    int ans = 0;
+    int ans1 = 0;
+    int ans2 = 0;
     string line;
 
     while (getline(in, line)) {
-        string first = "";
-        string last = "";
+        string first1 = "";
+        string last1 = "";
+        string first2 = "";
+        string last2 = "";
 
         for (int i = 0; i < line.size(); i++) {
             string num = "";
 
-            if (isdigit(line[i])) num = line[i];
+            if (isdigit(line[i])) {
+                num = line[i];
+                if (first1 == "") first1 = num;
+                if (first2 == "") first2 = num;
+                last1 = num;
+                last2 = num;
+                continue;
+            }
+
             // I hate this. It's slow, it's ugly, and it's slow
             if (i >= 2 && line.substr(i - 2, 3) == "one") num = "1";
             if (i >= 2 && line.substr(i - 2, 3) == "two") num = "2";
@@ -31,14 +42,16 @@ int main() {
             if (i >= 3 && line.substr(i - 3, 4) == "nine") num = "9";
 
             if (num == "") continue;
-            if (first == "") first = num;
-            last = num;
+            if (first2 == "") first2 = num;
+            last2 = num;
         }
 
-        ans += stoi(first + last);
+        ans1 += stoi(first1 + last1);
+        ans2 += stoi(first2 + last2);
     }
 
-    cout << ans;
+    cout << "Part 1: " << ans1 << endl;
+    cout << "Part 2: " << ans2 << endl;
 
     return 0;
 }
