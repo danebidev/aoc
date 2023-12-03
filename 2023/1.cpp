a3d106026e9b7b36372b2a2996f680c8ed2ec260
danebidev <57720273+danebidev@users.noreply.github.com>
2023-12-03 01:17:59 +0100

    2023 | Day 2 | Part 1

--- 2023/1.cpp
+++ 2023/1.cpp
@@ -1,5 +1,8 @@
 #include <bits/stdc++.h>
 
+#include <cctype>
+#include <string>
+
 using namespace std;
 
 int main() {
@@ -9,6 +12,30 @@ int main() {
     string line;
 
     while (getline(in, line)) {
+        string first = "";
+        string last = "";
+
+        for (int i = 0; i < line.size(); i++) {
+            string num = "";
+
+            if (isdigit(line[i])) num = line[i];
+            // I hate this. It's slow, it's ugly, and it's slow
+            if (i >= 2 && line.substr(i - 2, 3) == "one") num = "1";
+            if (i >= 2 && line.substr(i - 2, 3) == "two") num = "2";
+            if (i >= 4 && line.substr(i - 4, 5) == "three") num = "3";
+            if (i >= 3 && line.substr(i - 3, 4) == "four") num = "4";
+            if (i >= 3 && line.substr(i - 3, 4) == "five") num = "5";
+            if (i >= 2 && line.substr(i - 2, 3) == "six") num = "6";
+            if (i >= 4 && line.substr(i - 4, 5) == "seven") num = "7";
+            if (i >= 4 && line.substr(i - 4, 5) == "eight") num = "8";
+            if (i >= 3 && line.substr(i - 3, 4) == "nine") num = "9";
+
+            if (num == "") continue;
+            if (first == "") first = num;
+            last = num;
+        }
+
+        ans += stoi(first + last);
     }
 
     cout << ans;
---
+++ 2023/2.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+std::vector<std::string> split(const std::string &s, char delim) {
+    std::vector<std::string> result;
+    std::stringstream ss(s);
+    std::string item;
+
+    while (getline(ss, item, delim)) {
+        result.push_back(item);
+    }
+
+    return result;
+}
+
+int main() {
+    ifstream in("input");
+
+    int ans = 0;
+    int cur = 1;
+    string line;
+
+    while (getline(in, line)) {
+        line = line.substr(8);
+        int maxRed = 0, maxGreen = 0, maxBlue = 0;
+
+        vector<string> games = split(line, ';');
+        for (string game : games) {
+            vector<string> colors = split(game, ',');
+            for (string color : colors) {
+                int num;
+                stringstream(color) >> num;
+                color = color.substr(color.find_last_of(' ') + 1);
+
+                if (color == "red") maxRed = max(num, maxRed);
+                if (color == "green") maxGreen = max(num, maxGreen);
+                if (color == "blue") maxBlue = max(num, maxBlue);
+            }
+        }
+
+        if (maxRed <= 12 && maxGreen <= 13 && maxBlue <= 14) ans += cur;
+
+        cur++;
+    }
+
+    cout << ans;
+
+    return 0;
+}
