b89a90570d1aa6176c892bf171801c1df2804be6
danebidev <57720273+danebidev@users.noreply.github.com>
2023-12-14 19:12:10 +0100

    2023 | Day 4 | Part 1, 2

--- 2023/4.cpp
+++ 2023/4.cpp
@@ -14,17 +14,48 @@ vector<string> split(const string &s, char delim) {
     return result;
 }
 
+int getWinningNumbers(string numbers) {
+    vector<int> winning;
+    vector<string> nums = split(numbers, '|');
+
+    for (string num : split(nums[0], ' ')) {
+        if (!num.empty()) winning.push_back(stoi(num));
+    }
+
+    int res = 0;
+    for (string num : split(nums[1], ' ')) {
+        if (!num.empty() && find(winning.begin(), winning.end(), stoi(num)) != winning.end()) res++;
+    }
+
+    return res;
+}
+
 int main() {
     ifstream in("input");
 
-    int ans = 0;
+    int ans1 = 0;
+    int ans2 = 0;
+    map<int, int> scratchchards;
+    int cur = 1;
     string line;
 
     while (getline(in, line)) {
-        line = split(line, ':')[1];
+        int winning = getWinningNumbers(split(line, ':')[1]);
+        ans1 += pow(2, winning - 1);
+
+        scratchchards[cur] += 1;
+        while (winning > 0) {
+            scratchchards[cur + winning] += scratchchards[cur];
+            winning--;
+        }
+
+        ans2 += scratchchards[cur];
+
+        cur++;
     }
 
-    cout << ans;
+    cout << "Part 1: " << ans1 << endl;
+    cout << "Part 2: " << ans2 << endl;
 
     return 0;
 }
--- 2023/template.cpp
+++ 2023/template.cpp
@@ -5,13 +5,15 @@ using namespace std;
 int main() {
     ifstream in("input");
 
-    int ans = 0;
+    int ans1 = 0;
+    int ans2 = 0;
     string line;
 
     while (getline(in, line)) {
     }
 
-    cout << ans;
+    cout << "Part 1: " << ans1 << endl;
+    cout << "Part 2: " << ans2 << endl;
 
     return 0;
 }
